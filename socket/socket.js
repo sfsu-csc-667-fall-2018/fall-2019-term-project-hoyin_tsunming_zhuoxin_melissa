d56876ab9ea86bb88e164ee8947a947071fa46eb
Tevis <hoyin0929@gmail.com>
2019-12-16 00:04:19 -0800

    fixed game rooms chat bugs

--- socket/socket.js
+++ socket/socket.js
@@ -12,7 +12,6 @@ exports = module.exports = function(io) {
   var games = {};
 
   io.sockets.on("connection", function(socket) {
-
     socket.username =
       socket.handshake.session.userInfo.firstName +
       " " +
@@ -52,56 +51,88 @@ exports = module.exports = function(io) {
 
     socket.on("joined", function(roomId) {
       if (!games.hasOwnProperty(roomId)) {
-        games[roomId] = { players: 0, pid: [0, 0] };
+        games[roomId] = {
+          moves: [],
+          chatMessages: [],
+          players: 0,
+          pid: [0, 0]
+        };
       }
 
       socket.roomId = roomId;
+      socket.playerId = playerId;
       if (games[roomId].players < 2) {
+        //first two players will be participents.
         games[roomId].players++;
-        games[roomId].pid[games[roomId].players - 1] = playerId;
+        var Idx = games[socket.roomId].pid.indexOf(0); // assign empty index of array to player
+        games[roomId].pid[Idx] = playerId;
       } else {
         console.log("room full");
-        socket.emit("audience", true);
+        socket.emit("audience", true); // the players joined after 2nd will be audience/guests.
+        socket.audience = true;
         // return;
       }
 
-      console.log(games[roomId]);
       players = games[roomId].players;
 
-      if (players % 2 == 0) color = "black";
+      if (Idx == 1) color = "black";
+      // idx:0 (player one) will be while and idx:1 (player two and guests) will be black;
       //2nd player will be black
       else color = "white";
 
-      socket.emit("player", { playerId, players, color, roomId });
+      var preViousMoves = [];
+      if (games[roomId].moves.length > 0) {
+        preViousMoves = games[roomId].moves;
+      }
+
+      playerType = Idx + 1; //to display player number on the front page.
+      if (socket.audience) {
+        playerType = "Guest";
+      }
+
+      socket.join(roomId);
+
+      socket.emit("player", {
+        playerId,
+        players,
+        playerType,
+        color,
+        roomId,
+        preViousMoves,
+        chatMessages: games[roomId].chatMessages
+      });
       // players--;
     });
 
     socket.on("move", function(msg) {
+      games[socket.roomId].moves.push(msg.move);
       socket.broadcast.emit("move", msg);
     });
 
+    // user chat msg
+    socket.on("user_chat_msg", function(msgObj) {
+      games[socket.roomId].chatMessages.push(msgObj);
+      io.in(socket.roomId).emit("user_chat_msg_received", msgObj);
+    });
+
     socket.on("play", function(msg) {
-      socket.broadcast.emit("play", msg);
-      console.log("ready " + msg);
+      // socket.broadcast.emit("play", msg);
+      socket.to(socket.roomId).emit("play", msg);
     });
 
     socket.on("disconnect", function() {
-      // if (
-      //   games[socket.roomId].pid[0] == playerId ||
-      //   games[socket.roomId].pid[1] == playerId
-      // ) {
-      //   games[socket.roomId].players--;
-      //   socket.broadcast.emit("user_left", true);
-      //   console.log(games[socket.roomId]);
-      // }
       if (games.hasOwnProperty(socket.roomId)) {
-        games[socket.roomId].players--;
-        socket.broadcast.emit("user_left", true);
-        console.log(games[socket.roomId]);
+        if (!socket.audience) {
+          var Idx = games[socket.roomId].pid.indexOf(socket.playerId); // unset player array index on disconnect.
+          games[socket.roomId].pid[Idx] = 0;
 
-        if (games[socket.roomId].players == 0) {
-          console.log("room deleted");
-          delete games[socket.roomId];
+          games[socket.roomId].players--;
+          socket.broadcast.emit("user_left", true);
+
+          if (games[socket.roomId].players == 0) {
+            console.log("room deleted");
+            delete games[socket.roomId];
+          }
         }
       }
 
